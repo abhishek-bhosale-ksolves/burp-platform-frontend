788627b71f331c16907b16817db3fb483eb419e2
AbhishekASLK <abhishek.bhosale@ksolves.com>
2025-04-04 14:47:00 +0530

    removed unwanted var

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Logo from "../assets/burp.png";
 
-const SignUpForm = ({ onClose }) => {
+const SignUpForm = () => {
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
