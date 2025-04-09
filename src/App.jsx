a70637086156a6e3c5014442d1549639fa1d7601
AbhishekASLK <abhishek.bhosale@ksolves.com>
2025-04-09 18:01:40 +0530

    removed dummy positions

--- src/App.jsx
+++ src/App.jsx
@@ -13,31 +13,6 @@ import ReferralTable from "./components/pages/ReferralsPage";
 import UsersTable from "./components/pages/UsersPage";
 import "./index.css";
 
-const allPositions = [
-  {
-    title: "Salesforce Developer",
-    experience: "3 years",
-    description:
-      "We are looking for a skilled Salesforce Developer to join our team and drive the development, customization, and maintenance of our Salesforce platform.",
-  },
-  {
-    title: "React Developer",
-    experience: "2 years",
-    description: "Join our team to develop amazing React applications.",
-  },
-  {
-    title: "Node.js Developer",
-    experience: "2 years",
-    description: "We are looking for a Node.js Developer to join our team.",
-  },
-  {
-    title: "Senior Flutter Developer",
-    experience: "10 years",
-    description:
-      "We are looking for a skilled Senior Flutter Developer to join our team and drive the development, customization, and maintenance of our Flutter platform.",
-  },
-];
-
 const candidates = [
   {
     name: "John Doe",
