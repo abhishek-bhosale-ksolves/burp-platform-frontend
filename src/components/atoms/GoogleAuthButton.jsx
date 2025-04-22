a51ee2f99eec115331e7a9ef8f830cbf4186221a
AbhishekASLK <abhishek.bhosale@ksolves.com>
2025-04-22 13:45:51 +0530

    updated API func

--- src/components/atoms/GoogleAuthButton.jsx
+++ src/components/atoms/GoogleAuthButton.jsx
@@ -3,18 +3,16 @@ import { FcGoogle } from "react-icons/fc";
 
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "https://burp-platform-backend.onrender.com",
-  withCredentials: true,
-});
-
 const GoogleAuthButton = () => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const res = await API.get("/auth/api/user");
+        const res = await axios.get(
+          "https://burp-platform-backend.onrender.com/auth/api/user",
+          { withCredentials: true },
+        );
         setUser(res.data.user);
       } catch (err) {
         setUser(null);
@@ -32,50 +30,48 @@ const GoogleAuthButton = () => {
   };
 
   const handleLogout = async () => {
-    await API.get("/auth/api/logout");
-    setUser(null);
-    window.location.reload();
+    try {
+      await axios.get(
+        "https://burp-platform-backend.onrender.com/auth/api/logout",
+        { withCredentials: true },
+      );
+      setUser(null);
+      window.location.reload();
+    } catch (err) {
+      console.error("Logout failed:", err);
+    }
   };
-
-  console.log("**********");
-  console.log(user);
 
   return (
     <div>
       {user ? (
-        <>
-          <div className="flex items-center space-x-4">
-            <div className="text-black">
-              <span className="font-semibold">Welcome, {user.firstName}</span>
-              <span className="block text-xs text-right">
-                {user.role.charAt(0).toUpperCase() + user.role.slice(1)}
-              </span>
-            </div>
-            <img
-              src={user.photo}
-              alt="User"
-              className="rounded-full h-10 w-10"
-            />
-            <button
-              onClick={handleLogout}
-              style={{
-                backgroundColor: "#fff", // Google red
-                color: "black",
-                display: "flex",
-                border: "1px solid #ccc",
-                alignItems: "center",
-                gap: "10px",
-              }}
-            >
-              Logout
-            </button>
+        <div className="flex items-center space-x-4">
+          <div className="text-black">
+            <span className="font-semibold">Welcome, {user.firstName}</span>
+            <span className="block text-xs text-right">
+              {user.role.charAt(0).toUpperCase() + user.role.slice(1)}
+            </span>
           </div>
-        </>
+          <img src={user.photo} alt="User" className="rounded-full h-10 w-10" />
+          <button
+            onClick={handleLogout}
+            style={{
+              backgroundColor: "#fff",
+              color: "black",
+              display: "flex",
+              border: "1px solid #ccc",
+              alignItems: "center",
+              gap: "10px",
+            }}
+          >
+            Logout
+          </button>
+        </div>
       ) : (
         <button
           onClick={handleLogin}
           style={{
-            backgroundColor: "#fff", // Google red
+            backgroundColor: "#fff",
             color: "black",
             display: "flex",
             border: "1px solid #ccc",
